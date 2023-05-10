123fa78a190e55b06d16bb8e2e1cf2d4dae5894e
ujwal shakhakarmi <88956960+UjwalShakhakarmi@users.noreply.github.com>
2023-05-10 22:00:42 +0545

    Update Gamegui.java

--- src/Gamegui.java
+++ src/Gamegui.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class Gamegui {
+    JFrame frame;
+//    Car car;
+    Road road;
+    Gamegui(){
+        frame=new JFrame("Car game");
+//        frame.setSize(400,700);
+//        car=new Car();
+        road=new Road();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        frame.add(car);
+        frame.add(road);
+
+        frame.pack();
+        frame.setLayout(null);
+        frame.addKeyListener(road);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
 }
